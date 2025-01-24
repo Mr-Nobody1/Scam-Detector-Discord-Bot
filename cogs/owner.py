aaf4e74d40d78cf6aeceeff5557410edc678bdd0
Mr-Nobody1 <ammadali1188@gmail.com>
2025-01-24 22:47:32 +0500

    Refactor cogs/cat_meme.py to remove unused code and improve code structure

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,10 +1,4 @@
-"""
-Copyright © Krypton 2019-Present - https://github.com/kkrypt0nn (https://krypton.ninja)
-Description:
-🐍 A simple template to start to code your own and personalized Discord bot in Python
 
-Version: 6.2.0
-"""
 
 import discord
 from discord import app_commands
